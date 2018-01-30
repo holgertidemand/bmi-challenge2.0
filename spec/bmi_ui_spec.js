ce6fb31db98a375effbb8d4a6e16e11ab2940404
Holger_Tidemand <tidemand.holger@gmail.com>
2018-01-30 14:06:18 +0100

    added activ_system to show what system you wanna use

--- spec/bmi_ui_spec.js
+++ spec/bmi_ui_spec.js
@@ -17,3 +17,23 @@ describe('BMI_UI - index.html', function() {
         expect($('#display_message').text()).toBe('and you are Overweight');
     });
 });
+
+describe('BMI_UI - index.html', function() {
+    beforeEach(function() {
+        jasmine.getFixtures().fixturesPath = '.';
+        loadFixtures('index.html');
+        $.holdReady(false);
+        $('#imperial').trigger('click');
+        $('#weight').val('165'); // lbs
+        $('#height').val('71'); //inches
+        $('#calculate').trigger('click');
+    });
+
+    it("displays BMI Value", function() {
+        expect($('#display_value').text()).toBe('Your BMI is 23.01');
+    });
+
+    it("displays BMI Message", function() {
+        expect($('#display_message').text()).toBe('and you are Normal');
+    });
+});
